4b3ebcf3aa1f96bfd56b959ee804dda6232e8a87
Pyroblocks <68356572+Pyroblocks@users.noreply.github.com>
2021-06-02 19:00:07 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 questions =  [
-    {"id": 0, "question": "Bub.", "effects": {"right": 5}},
-    {"id": 1, "question": "Bub!", "effects": {"up": 5}},
-    {"id": 2, "question": "Bub?", "effects": {"scaleUp": 5}},
+    {"id": 0, "question": "Right?", "effects": {"right": 5}},
+    {"id": 1, "question": "Up?", "effects": {"up": 5}},
+    {"id": 2, "question": "Scale Up?", "effects": {"scaleUp": 5}},
 ];
