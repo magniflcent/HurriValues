11491ebfeb40ec0f0928da17d04299221d595676
Pyroblocks <68356572+Pyroblocks@users.noreply.github.com>
2021-06-10 16:18:35 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,4 +2,5 @@ questions =  [
     {"id": 0, "question": "Long term goals are more important than short term goals.", "effects": {"for": -1}},
     {"id": 1, "question": "Taxes are never justified.", "effects": {"soc": 1}},
     {"id": 2, "question": "Experience should be the most important factor in deciding who to vote for.", "effects": {"pop": 1}},
+    {"id": 3, "question": "The NGEC should be made public.", "effects": {"for": 1}},
 ];
