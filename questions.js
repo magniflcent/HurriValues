6c3fbea019e1556452e2c1fac0a33cc7adf4f2e8
Pyroblocks <68356572+Pyroblocks@users.noreply.github.com>
2021-06-10 17:06:37 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -3,7 +3,7 @@ questions =  [
     {"id": 1, "question": "The Supreme Court made the correct decision in the fourth election.", "effects": {"soc": -1}},
     {"id": 2, "question": "Experience should be the most important factor in deciding who to vote for.", "effects": {"pop": -1}},
     {"id": 3, "question": "DM campaigning should be allowed.", "effects": {"for": 1}},
-    {"id": 4, "question": "Taxes are never justified.", "effects": {"soc": 1}},
+    {"id": 4, "question": "We should not be afraid to introduce taxes if need be.", "effects": {"soc": 1}},
     {"id": 5, "question": "We should not be afraid to vote for members that have less experience than others.", "effects": {"pop": 1}},
     {"id": 6, "question": "We should give the people that are less active positions.", "effects": {"for": 1}},
     {"id": 7, "question": "The general community should be prioritized over individual rights.", "effects": {"soc": 1}},
