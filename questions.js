6d9b2daa2bc2c75a21cc1ed7f61f03c768834c62
Pyroblocks <68356572+Pyroblocks@users.noreply.github.com>
2021-06-10 16:29:13 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,11 @@
 questions =  [
     {"id": 0, "question": "Long term goals are more important than short term goals.", "effects": {"for": -1}},
-    {"id": 1, "question": "Taxes are never justified.", "effects": {"soc": 1}},
-    {"id": 2, "question": "Experience should be the most important factor in deciding who to vote for.", "effects": {"pop": 1}},
+    {"id": 1, "question": "The general community should be prioritized over individual rights.", "effects": {"soc": -1}},
+    {"id": 2, "question": "Experience should be the most important factor in deciding who to vote for.", "effects": {"pop": -1}},
     {"id": 3, "question": "The NGEC should be made public.", "effects": {"for": 1}},
+    {"id": 4, "question": "Taxes are never justified.", "effects": {"soc": 1}},
+    {"id": 5, "question": "We should not be afraid to vote for members that have less experience than others.", "effects": {"pop": 1}},
+    {"id": 6, "question": "We should give the people that are less active positions.", "effects": {"for": 1}},
+    {"id": 7, "question": "The Supreme Court made the incorrect decision in the fourth election.", "effects": {"soc": 1}},
+    {"id": 8, "question": "Staff members are favored to be voted into public positions.", "effects": {"pop": 1}},
 ];
