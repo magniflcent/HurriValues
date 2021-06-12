ddb318cde493f81069169226f6a3bda4853c97c1
Pyroblocks <68356572+Pyroblocks@users.noreply.github.com>
2021-06-12 11:35:16 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -5,7 +5,7 @@ questions =  [
     {"id": 3, "question": "March 25th and March 17th verified high risk.", "effects": {"wee": -1}, {"inn": 1}},
     {"id": 4, "question": "The SPC should redesign their website.", "effects": {"inn": 1}},
     {"id": 5, "question": "The National Weather Service needs more funding.", "effects": {"inn": 1}},
-    {"id": 6, "question": "Bassfield/Soso Tornado was incorrectly Assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
-    {"id": 7, "question": "Bassfield/Soso Tornado was incorrectly Assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
-    {"id": 8, "question": "Bassfield/Soso Tornado was incorrectly Assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
+    {"id": 6, "question": "April 12th, 2020 deserved to be categorized as high risk.", "effects": {"wee": 1}},
+    {"id": 7, "question": "El Reno was EF5 but didn't cause EF5 damage.", "effects": {"wee": 1}},
+    {"id": 8, "question": "The National Weather Service should go back to the Fujita Scale.", "effects": {"inn": -1}},
 ];
