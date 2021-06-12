4441ebd93ed1fe0ce8010532614c6e7227630672
Pyroblocks <68356572+Pyroblocks@users.noreply.github.com>
2021-06-12 11:41:42 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,7 +2,7 @@ questions =  [
     {"id": 0, "question": "The Bassfield/Soso Tornado was incorrectly assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
     {"id": 1, "question": "I am educated enough to issue accurate forecasts.", "effects": {"wee": -1}},
     {"id": 2, "question": "I am a Weenie.", "effects": {"wee": 1}},
-    {"id": 3, "question": "March 25th and March 17th verified high risk.", "effects": {"inn": 1}}
+    {"id": 3, "question": "March 25th and March 17th verified high risk.", "effects": {"inn": 1}},
     {"id": 4, "question": "The SPC should redesign their website.", "effects": {"inn": 1}},
     {"id": 5, "question": "The National Weather Service needs more funding.", "effects": {"inn": 1}},
     {"id": 6, "question": "April 12th, 2020 deserved to be categorized as high risk.", "effects": {"wee": 1}},
