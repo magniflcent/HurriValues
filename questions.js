39599f83da592b2ca93f30025835949bf191bda1
Pyroblocks <68356572+Pyroblocks@users.noreply.github.com>
2021-06-12 11:32:45 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,3 +1,11 @@
 questions =  [
-    {"id": 0, "question": "Bassfield/Soso Tornado was incorrectly Assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
+    {"id": 0, "question": "The Bassfield/Soso Tornado was incorrectly assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
+    {"id": 1, "question": "I am educated enough to issue accurate forecasts.", "effects": {"wee": -1}},
+    {"id": 2, "question": "I am a Weenie.", "effects": {"wee": 1}},
+    {"id": 3, "question": "March 25th and March 17th verified high risk.", "effects": {"wee": -1}, {"inn": 1}},
+    {"id": 4, "question": "Bassfield/Soso Tornado was incorrectly Assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
+    {"id": 5, "question": "Bassfield/Soso Tornado was incorrectly Assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
+    {"id": 6, "question": "Bassfield/Soso Tornado was incorrectly Assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
+    {"id": 7, "question": "Bassfield/Soso Tornado was incorrectly Assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
+    {"id": 8, "question": "Bassfield/Soso Tornado was incorrectly Assessed by the NWS and should have been EF5.", "effects": {"wee": 1}},
 ];
